edf9974d81306dd18872b5b29d4fcf55726ec5fa
Kevin Suaza <kevinsuazaproyectos@gmail.com>
2025-11-14 11:44:23 -0500

    add validations to SignUpRequestDTO

--- src/main/java/com/newbank/userservice/dto/SignUpRequestDTO.java
+++ src/main/java/com/newbank/userservice/dto/SignUpRequestDTO.java
@@ -5,11 +5,15 @@ import lombok.Setter;
 
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class SignUpRequestDTO {
     private String name;
+    @NotBlank(message = "email is required")
     private String email;
+    @NotBlank(message = "password is required")
     private String password;
     private List<PhoneRequestDTO> phones;
 }
